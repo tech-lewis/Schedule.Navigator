a1c4bd4a00644d039c3cb2b25b9791b366016374
Mark <techliujing@gmail.com>
2023-05-06 09:29:08 +0800

    内容修改

--- index.ios.js
+++ index.ios.js
@@ -4,71 +4,26 @@
  */
 
 import React, { Component } from 'react';
-import { StatusBarManager } from 'NativeModules';
-import Game2048 from "./2048/Game2048";
-import App from './Application/App';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View,
-  Button,
-  TouchableHighlight
+  View
 } from 'react-native';
 
 class AImageDemo extends Component {
-  state = {
-    switch: false,
-  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome} onPress = {(event) => { 
-          StatusBarManager.getHeight(statusH => {
-            alert(`Height=44px+${JSON.stringify(statusH)}`)
-          })
-        }}>
-          Game power by React Native v0.25!
-        </Text>
-        {
-          this.state.switch ? <Game2048/> : <App />
-        }
-        <TouchableHighlight onPress = { () => { 
-          const Dimensionsss = require('Dimensions')
-          const { width, height, scale } = Dimensionsss.get('window')
-          alert('分辨率:' + width + ', ' + height + ',' + scale)
-          this.setState({
-            switch: true
-          })
-        } }>
-          <View>
-            <Text style={styles.clickBtn}>Change</Text>
-          </View>
-        </TouchableHighlight>
-      </View>
-    );
-  }
-  render2() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome} onPress = {(event) => { alert('test')}}>
-          Welcome to React Native v0.25!
+        <Text style={styles.welcome}>
+          Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
         </Text>
-        <TouchableHighlight onPress = { () => { 
-          const Dimensionsss = require('Dimensions')
-          const { width, height, scale } = Dimensionsss.get('window')
-          alert('分辨率:' + width + ', ' + height + ',' + JSON.stringify(this.state)) 
-          this.setState({
-            switch: true
-          })
-        } }>
-          <View>
-            <Text style={styles.clickBtn}>Click me</Text>
-          </View>
-        </TouchableHighlight>
+        <Text style={styles.instructions}>
+          Shake or press menu button for dev menu
+        </Text>
       </View>
     );
   }
@@ -79,31 +34,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#ccc',
+    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 20,
-    borderWidth: 1,
-    borderColor: '#aaa',
-    borderRadius: 4,
-    padding: 15
+    margin: 10,
   },
   instructions: {
     textAlign: 'center',
-    color: '#cff',
+    color: '#333333',
     marginBottom: 5,
-  },
-
-  clickBtn: {
-    textAlign: 'center',
-    color: '#ff8000',
-    fontSize: 19,
-    width: 300,
-    height: 30,
-    marginTop: 5,
-    marginBottom: 34,
   },
 });
 
